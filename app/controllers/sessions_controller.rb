b744b49f943f99369c055d401f35829ba586798c
jyfan2004 <james.fan@kellogg.northwestern.edu>
2022-05-25 23:58:55 +0000

    completed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.find_by({"email" => params["email"]})
+        if @user
+            if BCrypt::Password.new(@user["password"]) == params["password"]
+                session["user_id"] = @user["id"]
+                flash["notice"] = "You've logged in."
+                redirect_to "/companies"
+            else
+                flash["notice"] = "Unsuccessful login."
+                redirect_to "/sessions/new"
+            end
+
+        else
+            flash["notice"] = "Unsuccessful login."
+            redirect_to "/sessions/new"
+        end
+
+    end
+
+    def destroy
+        flash["notice"] = "Goodbye."
+        session["user_id"] = nil
+        redirect_to "/sessions/new"
+    
+    end
+
+
+end
